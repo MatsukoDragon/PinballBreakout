a814be16be75bb1073aae2ec612c9d2f011dc8f9
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2019-07-13 12:07:56 +0900

    ボールの速度を制限

--- PinballBreakout/Assets/Script/Ball.cs
+++ PinballBreakout/Assets/Script/Ball.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+
+    //Vector3 XmaxSpeed (40.0f, 0f, 0f);
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("速度ベクトル:" + rb.velocity);
+
+        Debug.Log("速度:" + rb.velocity.magnitude);
+    }
+
+    
+    void FixedUpdate()
+    {
+        //速度がmaxSpeedを超えたなら
+        
         
     }
+    
 }
